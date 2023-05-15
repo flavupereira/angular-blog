19f4658d4ef523cb866da9326d7c43429f966b07
flavupereira <flavupereira@gmail.com>
2023-05-15 12:15:44 -0300

    content created

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -7,10 +7,12 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class SmallCardComponent implements OnInit {
   @Input ()
-  photoCover : string =" "
+  photoCover : string =""
 
   @Input ()
-  cardTitle : string= " "
+  cardTitle : string= ""
+  @Input ()
+  id:string="0"
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,29 @@
+export const dataFake =[
+{
+  "id":"1",
+  "title":"Marvel Anuncia novo Filme do Homem de Ferro",
+  "description": "olá",
+  "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/AD72115B9CDAE873B2234BD0908CEF7B45A2D9AD9C66C699239B46E582F70A8B/scale?width=1200&aspectRatio=1.78&format=jpeg",
+},
+
+{
+  "id":"2",
+  "title":"Novo Filme THOR",
+  "description": "bla bla bla",
+  "photoCover": "https://mixdeseries.com.br/wp-content/uploads/2022/07/Marvel-1.jpg",
+},
+
+{
+"id":"3",
+  "title":"CAPITÃO AMERICA",
+  "description": "bla bla bla",
+  "photoCover": "https://observatoriodocinema.uol.com.br/wp-content/uploads/2020/06/Captain-America.jpg",
+},
+{
+  "id":"4",
+    "title":"LANÇAMENTOS MARVEL",
+    "description": "bla bla bla",
+    "photoCover": "https://musicaecinema.com/wp-content/uploads/2022/12/marvel-scaled.jpg.webp",
+  }
+
+];
